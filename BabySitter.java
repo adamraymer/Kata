10e7bd3ddde86b0ec66fc65d1eb674aa7daabd0e
adamraymer <46364178+adamraymer@users.noreply.github.com>
2019-01-04 15:48:05 -0500

    Valid Range = True
    Simple class creation with time range check set to true.

---
+++ BabySitter.java
@@ -0,0 +1,16 @@
+package com.adamraymer.kata1;
+
+public class BabySitter {
+    public String startTime;
+    public String endTime;
+
+    public boolean checkStartTime (){
+        //this will check start time
+        return true;
+    }
+
+    public boolean checkEndTime (){
+        //this will check end time
+        return true;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.adamraymer.kata1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// test babysitter driver
+        BabySitter sitter = new BabySitter();
+
+
+        System.out.println(sitter.checkStartTime());
+        System.out.println(sitter.checkEndTime());
+
+    }
+}
