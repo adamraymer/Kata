1bc80233d0cf8dd7cd123518e7d68963b14451f5
adamraymer <46364178+adamraymer@users.noreply.github.com>
2019-01-04 21:10:13 -0500

    Start Time = valid
    This is added functionality for checking the start time to see if it fits in the correct range

--- BabySitter.java
+++ BabySitter.java
@@ -1,16 +1,56 @@
 package com.adamraymer.kata1;
 
 public class BabySitter {
-    public String startTime;
-    public String endTime;
+    private String startTime;
+    private String endTime;
+
+
+    public BabySitter (String enteredStartTime, String enteredEndTime){
+        startTime = enteredStartTime;
+        endTime = enteredEndTime;
+    }
 
     public boolean checkStartTime (){
         //this will check start time
-        return true;
+        //check value first
+        int checkStartTime = Integer.parseInt(startTime.substring(1,2));
+        String amOrPM;
+
+        //grab AM or PM
+
+        if (checkValidAMorPM(startTime.substring(5,7))){
+            amOrPM = startTime.substring(5,7);
+
+            if (amOrPM.contentEquals("PM")) {
+                //check PM range
+                if (validPMRange(checkStartTime)) {
+                    return true;
+                }
+            }
+        }
+
+            return false;
+
     }
 
     public boolean checkEndTime (){
         //this will check end time
         return true;
+    }
+
+   public boolean checkValidAMorPM (String value){
+        if (value.contentEquals("AM") || value.contentEquals("PM") ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean validPMRange (int value){
+        if ((value >= 5) && (value < 12)){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
