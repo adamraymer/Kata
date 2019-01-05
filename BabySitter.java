53988230d90d01c7c301e04a129ba0a0950f7fda
adamraymer <46364178+adamraymer@users.noreply.github.com>
2019-01-05 00:11:52 -0500

    Check more range, refactor
    I am adding more range checking functionality to check to see if the time range is in AM vs PM and if the range goes from evening to morning. I've also begun refactoring more to clean up the code. Getters and Setters were added to help clean up a lot of variable use as the number of checks increased.

--- BabySitter.java
+++ BabySitter.java
@@ -1,31 +1,98 @@
 package com.adamraymer.kata1;
 
 public class BabySitter {
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getCheckStartTime() {
+        return checkStartTime;
+    }
+
+    public void setCheckStartTime(int checkStartTime) {
+        this.checkStartTime = checkStartTime;
+    }
+
+    public int getCheckEndTime() {
+        return checkEndTime;
+    }
+
+    public void setCheckEndTime(int checkEndTime) {
+        this.checkEndTime = checkEndTime;
+    }
+
     private String startTime;
     private String endTime;
+    private String startAMorPM;
+    private String endAMorPM;
+    private int checkStartTime;
+    private int checkEndTime;
+
 
 
     public BabySitter (String enteredStartTime, String enteredEndTime){
         startTime = enteredStartTime;
         endTime = enteredEndTime;
+        startAMorPM = startTime.substring(5,7);
+        endAMorPM = endTime.substring(5,7);
+        checkStartTime = Integer.parseInt(startTime.substring(1,2));
+        checkEndTime = Integer.parseInt(endTime.substring(1,2));
+    }
+
+
+    public boolean checkTimeEntry (){
+        return true;
+
+    }
+
+
+    public boolean checkAMPMOrder (){
+        boolean checkOrder = true;
+        if (startAMorPM.contentEquals("AM") && endAMorPM.contentEquals("PM")){
+            checkOrder = false;
+        }
+
+        return checkOrder;
+    }
+
+    public boolean checkHourOrder () {
+        boolean checkOrder = false;
+
+        if ((startAMorPM.compareTo(endAMorPM) > 0)
+                && (getCheckStartTime() > getCheckEndTime())
+                && checkAMPMOrder ()) {
+            checkOrder = true;
+        }
+            return checkOrder;
     }
 
     public boolean checkStartTime (){
         //this will check start time
         //check value first
-        int checkStartTime = Integer.parseInt(startTime.substring(1,2));
-        String amOrPM;
 
-        //grab AM or PM
+        boolean checkStart = false;
 
-        if (checkValidAMorPM(startTime.substring(5,7))){
-            amOrPM = startTime.substring(5,7);
+        if (checkValidAMorPM(startAMorPM)){
 
-            if (amOrPM.contentEquals("PM")) {
+            if (startAMorPM.contentEquals("PM")) {
                 //check PM range
                 if (validPMRange(checkStartTime)) {
                     return true;
                 }
+            } else if (validAMRange(checkStartTime)) {
+                return true;
             }
         }
 
@@ -36,31 +103,25 @@ public class BabySitter {
     public boolean checkEndTime (){
         //this will check end time
         //check value first
-        int checkEndTime = Integer.parseInt(endTime.substring(1,2));
-        String amOrPM;
 
         //grab AM or PM
+        boolean checkTime = false;
+        if (checkValidAMorPM(endAMorPM)){
 
-        if (checkValidAMorPM(startTime.substring(5,7))){
-            amOrPM = endTime.substring(5,7);
-
-            if (amOrPM.contentEquals("AM")) {
-                //check AM range
-                if (validAMRange(checkEndTime)) {
-                    return true;
-                }
-            }
+            if (endAMorPM.contentEquals("PM")) {
+                //check PM range
+                if (validPMRange(checkEndTime)) {checkTime = true;}
+            } else if (validAMRange(checkEndTime)){checkTime = true;}
         }
 
-        return false;
+        return checkTime;
     }
 
    public boolean checkValidAMorPM (String value){
         if (value.contentEquals("AM") || value.contentEquals("PM") ){
             return true;
-        } else {
-            return false;
         }
+            return false;
     }
 
     public boolean validPMRange (int value){
